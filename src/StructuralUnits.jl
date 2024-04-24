31177f4e87f81ffbda758a5ea4f7729ea1c587b5
co1emi11er2 <colemiller2010@gmail.com>
2024-04-23 23:25:49 -0500

    Update units

--- src/StructuralUnits.jl
+++ src/StructuralUnits.jl
@@ -3,6 +3,7 @@ module StructuralUnits
 # Write your package code here.
 using Unitful
 
+Unitful.register(StructuralUnits)
 @unit kip "kip" Kip 1000*u"lbf" false
 @unit ksi "ksi" KSI 1*u"kip"/(1*u"inch^2") false
 @unit ksf "ksf" KSF 1*u"kip"/(1*u"ft^2") false
@@ -11,19 +12,20 @@ Unitful.preferunits(u"ft")
 
 @derived_dimension Area Unitful.𝐋^2
 @derived_dimension Volume Unitful.𝐋^3
-@derived_dimension Force Unitful.𝐋^2*Unitful.𝐌*Unitful.𝐓^-2
+@derived_dimension Force Unitful.𝐋*Unitful.𝐌*Unitful.𝐓^-2
 @derived_dimension Moment Unitful.𝐋^2*Unitful.𝐌*Unitful.𝐓^-2
 @derived_dimension Stress Unitful.𝐋*Unitful.𝐌*Unitful.𝐓^-2/Unitful.𝐋^2
 
-Unitful.promote_unit(::S, ::T) where {S<:Main.AreaUnits, T<:Main.AreaUnits} = u"inch^2"
-Unitful.promote_unit(::S, ::T) where {S<:Main.AreaUnits, T<:Main.AreaUnits} = u"ft^3"
-Unitful.promote_unit(::S, ::T) where {S<:Main.ForceUnits, T<:Main.ForceUnits} = u"kip"
-Unitful.promote_unit(::S, ::T) where {S<:Main.MomentUnits, T<:Main.MomentUnits} = u"kip*ft"
-Unitful.promote_unit(::S, ::T) where {S<:Main.StressUnits, T<:Main.StressUnits} = u"ksi"
+Unitful.promote_unit(::S, ::T) where {S<:StructuralUnits.AreaUnits, T<:StructuralUnits.AreaUnits} = u"inch^2"
+Unitful.promote_unit(::S, ::T) where {S<:StructuralUnits.VolumeUnits, T<:StructuralUnits.VolumeUnits} = u"ft^3"
+Unitful.promote_unit(::S, ::T) where {S<:StructuralUnits.ForceUnits, T<:StructuralUnits.ForceUnits} = u"kip"
+Unitful.promote_unit(::S, ::T) where {S<:StructuralUnits.MomentUnits, T<:StructuralUnits.MomentUnits} = u"kip*ft"
+Unitful.promote_unit(::S, ::T) where {S<:StructuralUnits.StressUnits, T<:StructuralUnits.StressUnits} = u"ksi"
+
 
 const localpromotion = copy(Unitful.promotion)
 function __init__()
-    Unitful.register(YourModule)
+    Unitful.register(StructuralUnits)
     merge!(Unitful.promotion, localpromotion)
 end
 
