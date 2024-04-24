0df29c943597fdef4e76b49259ac1b35118e17b8
Documenter.jl <documenter@juliadocs.github.io>
2024-04-24 04:27:40 +0000

    build based on 31177f4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StructuralUnits","category":"page"},{"location":"#StructuralUnits","page":"Home","title":"StructuralUnits","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for StructuralUnits.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StructuralUnits]","category":"page"}]
+}
